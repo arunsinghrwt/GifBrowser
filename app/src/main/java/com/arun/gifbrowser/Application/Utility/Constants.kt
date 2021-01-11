bf6f9efde45493d81fbf6c8fe73f4cd1b6ba55db
Arun Singh Rawat <arunsinghrawatrawat@gmail.com>
2021-01-12 00:16:32 +0530

    new commit

--- app/src/main/java/com/arun/gifbrowser/Application/Utility/Constants.kt
+++ app/src/main/java/com/arun/gifbrowser/Application/Utility/Constants.kt
@@ -6,7 +6,7 @@ package com.arun.gifbrowser.Application.Utility
  */
 object Constants {
      val BASE_URL: String = " https://api.giphy.com/"
-     val API_KEY : String = ""
+     val API_KEY : String = "ZOGwSJ26NoSq6Ms540hblZnxZ19KCPuX"
      var HeightPX = 0
      var WidthPX = 0
      var DENSITY = 0f
